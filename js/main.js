5adcc57210059249f7661e9dad2ffaa09002a578
unknown <luanfarias83@gmail.com>
2020-01-01 18:39:48 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+                //AULA 1
+/*Tipagem dinamica,
+ou seja, não é necessário dizer qual o tipo de variável*/
+//var nome = "Luan Farias";
+//var n1 = 21;
+//var n2 = 10;
+//var frase = "Japão é o melhor time do mundo!"
+
+//alert(nome + " tem " + idade + " anos! "); //String concatenadas que exibem mensagem
+//alert(idade + idade2); //Soma de 2 variaveis
+//console.log(nome); //Mostra na aba console do Dev googlechrome
+//console.log(n1 + n2); 
+//console.log(n1 * n2);
+//console.log(n1 - n2);
+//console.log(n1 / n2);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+//console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());
+                    //AULA 1 FIM
+    //AULA 2 - Arrays e dicionário
+var lista = ["Maçã", "Pêra", "Laranja"] //Array
+//console.log(lista); //Imprime o array todo
+//console.log(lista[1]);
+alert(lista[1]);
