bf28c5c948c96a6d5135de3aa15ce234fcd3b95b
root <luanfarias83@gmail.com>
2020-01-16 00:10:23 -0300

    Até a aula de funções

--- js/main.js
+++ js/main.js
@@ -19,7 +19,61 @@ ou seja, não é necessário dizer qual o tipo de variável*/
 //console.log(frase.toLowerCase());
                     //AULA 1 FIM
     //AULA 2 - Arrays e dicionário
-var lista = ["Maçã", "Pêra", "Laranja"] //Array
+//var lista = ["Maçã", "Pêra", "Laranja"] //Array começa com posição 0, 1, 2, 3, ...
 //console.log(lista); //Imprime o array todo
 //console.log(lista[1]);
-alert(lista[1]);
+//alert(lista[1]);
+//lista.push("Uva"); //Adiciona um item ao Array
+//lista.pop(); // Remove o ultimo item do array
+//console.log(lista.length); Imprime o tamanho do array
+//console.log(lista.reverse()); //Imprime lista no formato reverso
+//console.log(lista[0]);
+//console.log(lista.toString()[0]); //Função que transforma Array em String
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" ~ "));
+
+//var fruta = {nome: "maça", cor: "vermelha"}
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+//var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+//console.log(frutas); //imprime a variavel fruta e o nome
+//alert(frutas[1].nome); // imprime a variavel fruta e a cor
+
+    //AULA 3 - Condicionais, laços de repetição e Date
+/*
+var idade = prompt("Qual a sua idade");
+if (idade >= 18){
+    alert("Ta Ficando velho hein");
+}
+else{
+    alert("Ta zero bala");
+};
+*/
+
+/*
+var count = 0;
+while (count <= 5 ){
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var count;
+for(count = 0; count <= 5; count++){
+    alert(count)
+}
+*/
+/*
+var d = new Date();
+alert("Segundos: " + d.getSeconds());
+alert("Minutos: " + d.getMinutes());
+alert("Horas:" + d.getHours());
+alert("Dia: " + d.getDay());
+alert("Mês: " + d.getMonth()+1);
+alert("Ano: " + d.getFullYear());
+*/
+    //AULA 4 - Desenvolva págians Web com JS
+
